fb9044df18527b5c05608eafd57e69c473da5c69
Hari prasad <hariprasadyadavjangili@gmail.com>
2025-08-05 10:03:59 +0530

    working on the reply of my assistant

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pyttsx3
 import speech_recognition as sr
-import webbrowser
-import datetime
-import pyjokes
+# import webbrowser
+# import datetime
+# import pyjokes
 
 # this part of code will recognise what we speak 
-def sptext():
+def input():
     recognizer=sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
@@ -19,24 +19,35 @@ def sptext():
             print("Sorry, i could not match you , say again")
 
 # this part of code will speak 
-def spechtx(x):
+def speak(x):
     engine = pyttsx3.init()
 
     voices = engine.getProperty("voices")
     engine.setProperty("voice",voices[0].id)  #[0] = male voice , [1] = female voice
 
     rate = engine.getProperty("rate")
-    engine.setProperty("rate" , 170)  #setting the voice speed of assistant
-    
+    engine.setProperty("rate" , 155)  #setting the voice speed of assistant
+
+    print("Assistant says:", x)      # ✅ Print the same text being spoken
     engine.say(x)
     engine.runAndWait()
+   
 
 
 # 
+data1 = input().lower()
 if __name__ == '__main__' :
+    
+    if "your name" in data1 :
+        name = "my name is jarvis , your personal assistant"
+        speak(name)
 
-    if sptext().lower() == "hey jarvis":
-        print('Hello User , How can i help you..')
-    else:
-        print("sorry , i did'nt get you ")
         
+
+    if "hey jarvis" in data1 :
+        reply = "Hello User , How can i help you"
+        speak(reply)
+    
+
+    # else:
+    #     print("sorry , i did'nt get you ")
